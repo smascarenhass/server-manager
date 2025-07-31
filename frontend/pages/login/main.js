8bf32d1fa763ae11b119602654abbf6314b43f18
Eric Telhado <eric.telhado1209@gmail.com>
2025-07-31 11:38:14 -0300

    feat: login page

--- frontend/pages/login/main.js
+++ frontend/pages/login/main.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('.login-form');
 const submit = document.getElementById('submit');
 
+const accounts = {
+    'username': '123',	
+    'password': '123'
+};
+
 // Previne o comportamento padrão do formulário e faz o login ao submeter
 submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -13,11 +18,19 @@ submit.addEventListener("click", (e) => {
             'password': password 
         }
     );//juntando o usuário e a senha em um array
-    console.log(users);
     
     // Limpa os campos de input
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
+    
+    if (username === accounts.username && password === accounts.password) {
+        // Redireciona para a página home após o login
+        window.location.href = '../home/index.html';
+       
+    } else {
+        alert('Usuário ou senha inválidos');
+        window.location.reload();
+    }
 });
 
 form.addEventListener("keypress", (e) => {
